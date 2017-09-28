9c60661705df6594ac5368d0e4ffb54008b4f017
Andy Gaines <andyagaines@gmail.com>
2017-09-28 13:22:52 -0500

    finished prob 1

--- main.js
+++ main.js
@@ -10,17 +10,14 @@
 
 function replacePicture() {
 
-	console.log('test');
-
-  $('.thumbnail').on('click', (event) => {
-
-		console.log(event);
-
-    let newSrc = $(event.Target).find('img').attr('src');
-    let newAlt = $(event.Target).find('img').attr('alt');
-
+  $('.thumbnail').on('click', function(event) {
+	
+    let newSrc = $(event.currentTarget).find('img').attr('src');
+    let newAlt = $(event.currentTarget).find('img').attr('alt');
+			
+	
     $('.hero img').attr('src',newSrc).attr('alt',newAlt);
   });
 }
-	
-replacePicture();
+		
+$(replacePicture());
